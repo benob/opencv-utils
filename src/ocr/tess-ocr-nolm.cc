17569bb639b7bfbaf9b1834f830f05bd22940c0c
Benoit Favre <benoit.favre@lif.univ-mrs.fr>
2013-07-29 19:09:13 +0200

    ported to new options; added support for multi-binarization

--- src/ocr/tess-ocr-nolm.cc
+++ src/ocr/tess-ocr-nolm.cc
@@ -16,6 +16,28 @@
 // accurate frame reading
 #include "repere.h"
 
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+                    return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+            s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+                    return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+            return ltrim(rtrim(s));
+}
+
 namespace amu {
 
     class Rect {
@@ -26,11 +48,15 @@ namespace amu {
 
             Rect() : start(0), end(0), x(0), y(0), width(0), height(0) { }
 
-            Rect(const Rect& other, int factor = 1) : start(other.start), end(other.end), x(other.x * factor), y(other.y * factor), width(other.width * factor), height(other.height * factor), text(other.text) { }
+            Rect(const Rect& other, double factor = 1) : start(other.start), end(other.end), x(other.x * factor), y(other.y * factor), width(other.width * factor), height(other.height * factor), text(other.text) { }
 
             Rect(int _x, int _y, int _width, int _height) : start(0), end(0), x(_x), y(_y), width(_width), height(_height) { }
 
             Rect(double _start, double _end, int _x, int _y, int _width, int _height, std::vector<std::string> _text = std::vector<std::string>()) : start(_start), end(_end), x(_x), y(_y), width(_width), height(_height), text(_text) { }
+
+            operator cv::Rect() {
+                return cv::Rect(x, y, width, height);
+            }
 
             static double TimeFromString(const std::string &text) {
                 // PT1M57.040S
@@ -137,11 +163,11 @@ namespace amu {
         int imageWidth, imageHeight;
         tesseract::TessBaseAPI tess; 
         public:
-        OCR(const std::string& datapath="") : imageWidth(0), imageHeight(0) {
+        OCR(const std::string& datapath="", const std::string& lang="fra") : imageWidth(0), imageHeight(0) {
             if(datapath != "") {
                 setenv("TESSDATA_PREFIX", std::string(datapath + "/../").c_str(), 1);
             }
-            tess.Init("OCR", "fra", tesseract::OEM_DEFAULT); 
+            tess.Init("OCR", lang.c_str(), tesseract::OEM_DEFAULT); 
             SetMixedCase();
         }
 
@@ -189,12 +215,13 @@ namespace amu {
             tess.Recognize(NULL);
             const char* text = tess.GetUTF8Text();
             result.text = ProtectNewLines(std::string(text));
+            trim(result.text);
             delete text;
             result.confidence = tess.MeanTextConf() / 100.0;
             return result;
         }
 
-        Result RefineAndProcess(amu::Rect rect, int factor = 1, int y_start = -4, int y_end = 4, int y_step = 2) {
+        Result RefineAndProcess(amu::Rect rect, double factor = 1, int y_start = -4, int y_end = 4, int y_step = 2) {
             Result argmax;
             int argmax_q = 0;
             int argmax_d = 0;
@@ -231,90 +258,72 @@ namespace amu {
 
 int main(int argc, char** argv) {
 
-    int zoom = 2; // zoom factor
-    bool refine = false; // try to find better rectangles 
-    bool upper_case = false;
-    int y_start = -3, y_end = 3, y_step = 3;
-    std::string frameList = "";
-    std::string index = "";
-    std::string frameDir = "";
-    std::string dataPath = "";
+    amu::CommandLine options(argv, "[options]\n");
+    options.AddUsage("  --data <directory>                tesseract model directory (containing tessdata/)\n");
+    options.AddUsage("  --lang <language>                 tesseract model language (default fra)\n");
+    options.AddUsage("  --upper-case                      contains only upper case characters\n");
+    options.AddUsage("  --sharpen                         sharpen image before processing\n");
+    options.AddUsage("  --show                            show image beeing processed\n");
 
-    if(argc < 4) {
-        std::cerr << "usage: " << argv[0] << " [options] <frame-list> <idx> <frame-dir>" << std::endl;
-        std::cerr << "--zoom <int>                  zoom factor before performing OCR (default=" << zoom << ")\n";
-        std::cerr << "--refine <from> <to> <step>   refine input rectangle to get better confidence (example -3 3 3)\n";
-        std::cerr << "--upper-case                  only allow for upper case characters\n";
-        std::cerr << "--data <director>             set model directory\n";
-        return 1;
-    }
+    double zoom = options.Read("--scale", 1); // from video.Configure()
+    std::string dataPath = options.Get<std::string>("--data", "");
+    std::string lang = options.Get<std::string>("--lang", "fra");
+    bool upper_case = options.IsSet("--upper-case");
+    bool sharpen = options.IsSet("--sharpen");
+    bool show = options.IsSet("--show");
 
-    for(int i = 1; i < argc; i++) {
-        std::string argument = argv[i];
-        if(argument == "--zoom") {
-            zoom = strtol(argv[i + 1], NULL, 10);
-            i++;
-        } else if(argument == "--refine") {
-            y_start = strtol(argv[i + 1], NULL, 10);
-            y_end = strtol(argv[i + 2], NULL, 10);
-            y_step = strtol(argv[i + 3], NULL, 10);
-            refine = true;
-            i += 3;
-        } else if(argument == "--upper-case") {
-            upper_case = true;
-        } else if(argument == "--data") {
-            dataPath = argv[i + 1];
-            i++;
-        } else if(frameList == "") {
-            frameList = argv[i];
-        } else if(index == "") {
-            index = argv[i];
-        } else if(frameDir == "") {
-            frameDir = argv[i];
-        }
-    }
-
-    std::cerr << "zoom        = " << zoom << "\n";
-    std::cerr << "refine      = " << refine << " " << y_start << ":" << y_end << ":" << y_step << "\n";
-    std::cerr << "uppercase   = " << upper_case << "\n";
-    std::cerr << "frame list  = " << frameList << "\n";
-    std::cerr << "index       = " << index << "\n";
-    std::cerr << "frame dir   = " << frameDir << "\n";
-    std::cerr << "data path   = " << dataPath << "\n";
-
-    amu::OCR ocr(dataPath);
+    amu::OCR ocr(dataPath, lang);
     if(upper_case) ocr.SetUpperCase();
 
-    amu::CommandLine options(argv);
     amu::VideoReader video;
     if(!video.Configure(options)) return 1;
-
-    //int numFrames = capture.get(CV_CAP_PROP_FRAME_COUNT);
-    /*int frameWidth = capture.get(CV_CAP_PROP_FRAME_WIDTH);
-    int frameHeight = capture.get(CV_CAP_PROP_FRAME_HEIGHT);*/
-    amu::Idx idx(index);
+    if(options.Size() != 0) options.Usage();
 
     std::vector<std::string> remaining;
     std::vector<amu::Rect> rects = amu::Rect::ReadAll(std::cin, remaining);
     std::stable_sort(rects.begin(), rects.end(), amu::RectLess());
     std::cerr << "read " << rects.size() << " rectangles\n";
 
-    // read rectangles from stdin (see get-rectangles.py companion script) -> proper xml reading would be better
-    // process all frames in order (opencv screws up on some videos if we jump to a specific frame
     size_t current = 0;
     int frame = 0;
 
     cv::Mat resized;
 
     for(current = 0; current < rects.size(); current++) {
-        video.SeekTime(rects[current].start);
+        video.SeekTime((rects[current].start + rects[current].end) / 2);
         while(video.HasNext() && video.GetTime() <= rects[current].end) {
             video.ReadFrame(resized);
-            ocr.SetImage(resized);
+            if(show) cv::imshow("original", resized(amu::Rect(rects[current], zoom)));
+
+            if(sharpen) {
+                cv::Mat blured;
+                cv::GaussianBlur(resized, blured, cv::Size(0, 0), 3);
+                cv::addWeighted(resized, 1.5, blured, -0.5, 0, blured);
+            }
+            cv::cvtColor(resized, resized, CV_BGR2GRAY);
+            
+            cv::Mat cropped;
+            double threshold =cv::threshold(resized(amu::Rect(rects[current], zoom)), cropped, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);
+            double ratio = (double) cv::countNonZero(cropped) / (cropped.rows * cropped.cols);
+            if(ratio < 0.5) cv::bitwise_not(cropped, cropped);
+
+            ocr.SetImage(cropped);
             amu::Result result;
-            ocr.SetRectangle(amu::Rect(rects[current], zoom));
-            if(refine) result = ocr.RefineAndProcess(rects[current], zoom, y_start, y_end, y_step);
-            else result = ocr.Process();
+            result.confidence = 0;
+            cv::Mat argmax;
+
+            for(int i = threshold - 32; i <= threshold + 32; i += 8) {
+                cv::threshold(resized(amu::Rect(rects[current], zoom)), cropped, i, 255, cv::THRESH_BINARY);
+                ocr.SetImage(cropped);
+                amu::Result current = ocr.Process();
+                if(current.confidence > result.confidence) {
+                    result = current;
+                    cropped.copyTo(argmax);
+                }
+            }
+            //ocr.SetRectangle(amu::Rect(rects[current], zoom));
+            //if(refine) result = ocr.RefineAndProcess(rects[current], zoom, y_start, y_end, y_step);
+            //result = ocr.Process();
             for(size_t i = 0; i < rects[current].text.size(); i++) {
                 if(amu::Rect::StartsWith(rects[current].text[i], "<StringInfo name=\"Text\">")) {
                         //std::cout << rects[current].text[i] << "\n";
@@ -324,6 +333,10 @@ int main(int argc, char** argv) {
                 } else {
                     std::cout << rects[current].text[i] << "\n";
                 }
+            }
+            if(show) {
+                cv::imshow("binarized", argmax);
+                cv::waitKey(0);
             }
         }
         //std::cerr << frame << "/" << numFrames <<"\n";
