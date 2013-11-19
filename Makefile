0cd32de2ebd0f4c5859fb72d1f415c1c61f2b388
Benoit Favre <benoit.favre@lif.univ-mrs.fr>
2013-11-19 12:41:42 +0100

    add combination of face detectors

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 OPT:=-O2 -pipe 
 CXXFLAGS:=`pkg-config opencv --cflags` -I. -Iinclude -g -MMD -Wall $(OPT) -flto
-LDFLAGS:=-Wl,--no-as-needed -lavcodec -lavformat -lavutil -lswscale `pkg-config opencv --libs` -lm -lexpat -ltesseract -flto -lcurses
+LDFLAGS:=-Wl,--no-as-needed -lavcodec -lavformat -lavutil -lswscale `pkg-config opencv --libs` -lm -lexpat -ltesseract -flto -lcurses -lfann
 CXX=clang++
 
 PROGS:=extract-features-ref make-ref predict-bow shot-boundary-detector view-shot-boundaries subshot-detector temporal-median tess-ocr train-bow haar-detector view-ascii hog-view extract-hog cumulative-gradients subshot-from-template extract-hog-subshot track-faces-in-shots get-hog-training-data detect-from-hog get-hog-extra-negatives classify-hog-subshot detector-performance
--- src/faces/detector-performance.cc
+++ src/faces/detector-performance.cc
@@ -3,6 +3,55 @@
 #include "repere.h"
 #include "xml.h"
 #include "face.h"
+#include <floatfann.h>
+
+namespace amu {
+    class FannClassifier {
+        private:
+            struct fann* ann;
+        public:
+            FannClassifier(const std::string& filename = "") : ann(NULL) {
+                if(filename != "") Load(filename);
+            }
+            ~FannClassifier() {
+                if(ann != NULL) fann_destroy(ann);
+            }
+
+            bool Load(const std::string& filename) {
+                ann = fann_create_from_file(filename.c_str());
+                return ann != NULL;
+            }
+
+            double Classify(const std::vector<float>& descriptors) const {
+                fann_type input[descriptors.size()];
+                for(size_t i = 0; i < descriptors.size(); i++) {
+                    input[i] = descriptors[i];
+                }
+                fann_type* output = fann_run(ann, input);
+                return output[0];
+            }
+    };
+}
+
+double skin(cv::Mat &image) {
+    cv::Mat hsv;
+    cv::cvtColor(image, hsv, CV_BGR2HSV);
+    cv::Mat hue(image.size(), CV_8UC1), saturation(image.size(), CV_8UC1), value(image.size(), CV_8UC1);
+    std::vector<cv::Mat> channels;
+    channels.push_back(hue);
+    channels.push_back(saturation);
+    channels.push_back(value);
+    cv::split(hsv, channels);
+    cv::threshold(hue, hue, 18, 255, CV_THRESH_BINARY_INV);
+    cv::threshold(saturation, saturation, 50, 255, CV_THRESH_BINARY);
+    cv::threshold(value, value, 80, 255, CV_THRESH_BINARY);
+    cv::Mat output;
+    cv::bitwise_and(hue, saturation, output);
+    cv::bitwise_and(output, value, output);
+    cv::Scalar sums = cv::sum(output);
+    double result = sums[0] / (output.rows * output.cols * 255);
+    return result;
+}
 
 bool ReadLibLinearModel(const std::string& filename, std::vector<float>& model) {
     std::fstream modelStream(filename.c_str());
@@ -20,7 +69,6 @@ bool ReadLibLinearModel(const std::string& filename, std::vector<float>& model) 
             float value;
             reader >> value;
             model.push_back(value);
-            std::cout << value << "\n";
         }
     }
     return true;
@@ -77,11 +125,15 @@ int main(int argc, char** argv) {
     options.AddUsage("  --model <model-file>              add negatives from wrong detections by previous model \n");
     options.AddUsage("  --groups <num>                    minimum number of detections that need to be clustered together to fire (default=2)\n");
     options.AddUsage("  --threshold <threshold>           score decision threshold (default=0)\n");
+    options.AddUsage("  --haar <path>                     path to haar model directory\n");
+    options.AddUsage("  --ann <path>                      path to fann model\n");
     options.AddUsage("  --show                            show detections\n");
     std::string xgtfFilename = options.Get<std::string>("--xgtf", "");
     int window = options.Get("--window", 32);
     double grow = options.Get("--grow", 1.5);
     std::string hogModelFile = options.Get<std::string>("--model", "");
+    std::string haarPath = options.Get<std::string>("--haar", "");
+    std::string annPath = options.Get<std::string>("--ann", "");
     double hitThreshold = options.Get("--threshold", 0.0);
     int groupThreshold = options.Get("--groups", 2);
     double scale = options.Read("--scale", 1.0);
@@ -89,12 +141,13 @@ int main(int argc, char** argv) {
 
     amu::VideoReader video;
     if(!video.Configure(options)) return 1;
-    if(options.Size() != 0 || xgtfFilename == "" || hogModelFile == "") options.Usage();
+    if(options.Size() != 0 || xgtfFilename == "" || hogModelFile == "" || haarPath == "" || annPath == "") options.Usage();
 
     std::vector<Person> persons;
     ReadXgtf(xgtfFilename.c_str(), persons);
 
     cv::HOGDescriptor hog(cv::Size(window, window), cv::Size(16,16), cv::Size(8,8), cv::Size(8,8), 9);
+    amu::FannClassifier ann(annPath);
 
     if(hogModelFile != "") {
         std::vector<float> model;
@@ -107,7 +160,9 @@ int main(int argc, char** argv) {
         frames[persons[p].keyFrame].push_back(p);
     }
 
-    double numRef = 0, numHyp = 0, numCorrect = 0;
+    cv::CascadeClassifier haarDetector(haarPath + "/haarcascade_frontalface_alt.xml");
+
+    double numRef = 0, numHyp = 0, numCorrect = 0, numHypHaar = 0, numCorrectHaar = 0, numHypBoth = 0, numCorrectBoth = 0;
 
     cv::Mat image;
     cv::Size size = video.GetSize();
@@ -135,29 +190,70 @@ int main(int argc, char** argv) {
             person.rect = centered;
             if(show) cv::rectangle(image, centered, cv::Scalar(0, 255, 0), 1);
         }
-        if(hogModelFile != "") {
-            std::vector<cv::Rect> found;
-            cv::Size padding(cv::Size(0, 0));
-            cv::Size winStride(cv::Size(4, 4));
-            hog.detectMultiScale(image, found, hitThreshold, winStride, padding, 1.1, groupThreshold);
-            for(size_t i = 0; i < found.size(); i++) {
-                //if(found[i].width < 64) continue;
-                if(show) cv::rectangle(image, found[i], cv::Scalar(0, 0, 255), 1);
-                bool correct = false;
-                for(size_t p = 0; p < frame->second.size(); p++) {
-                    if((persons[frame->second[p]].rect & found[i]).area() > persons[frame->second[p]].rect.area() / 2
-                            && (persons[frame->second[p]].rect & found[i]).area() > found[i].area() / 2) {
-                        correct = true;
-                        break;
-                    }
+
+        std::vector<cv::Rect> found;
+        cv::Size padding(cv::Size(0, 0));
+        cv::Size winStride(cv::Size(4, 4));
+        hog.detectMultiScale(image, found, hitThreshold, winStride, padding, 1.1, groupThreshold);
+        for(size_t i = 0; i < found.size(); i++) {
+            //if(found[i].width < 64) continue;
+            cv::Mat face(image, found[i]);
+            cv::resize(face, face, cv::Size(window, window));
+            std::vector<float> descriptorsValues;
+            std::vector<cv::Point> locations;
+            hog.compute(face, descriptorsValues, cv::Size(0,0), cv::Size(0,0), locations);
+            double score = ann.Classify(descriptorsValues);
+            std::cout << found[i] << " " <<score << "\n";
+            if(score < 0.5) continue;
+
+            if(show) cv::rectangle(image, found[i], cv::Scalar(0, 0, 255), 1);
+            bool correct = false;
+            for(size_t p = 0; p < frame->second.size(); p++) {
+                if((persons[frame->second[p]].rect & found[i]).area() > persons[frame->second[p]].rect.area() / 2
+                        && (persons[frame->second[p]].rect & found[i]).area() > found[i].area() / 2) {
+                    correct = true;
+                    break;
                 }
-                if(correct) {
-                    numCorrect++;
-                }
-                numHyp++;
             }
-            numRef += frame->second.size();
+            if(correct) {
+                numCorrect++;
+                numCorrectBoth++;
+            }
+            numHyp++;
+            numHypBoth++;
         }
+        std::vector<cv::Rect> haarFound;
+        haarDetector.detectMultiScale(image, haarFound);
+        for(size_t i = 0; i < haarFound.size(); i++) {
+            cv::Mat face(image, haarFound[i]);
+            if(skin(face) < 0.3) continue;
+            if(show) cv::rectangle(image, haarFound[i], cv::Scalar(255, 0, 255), 1);
+            bool correct = false;
+            for(size_t p = 0; p < frame->second.size(); p++) {
+                if((persons[frame->second[p]].rect & haarFound[i]).area() > persons[frame->second[p]].rect.area() / 4
+                        && (persons[frame->second[p]].rect & haarFound[i]).area() > haarFound[i].area() * .8) {
+                    correct = true;
+                    //if(show) cv::rectangle(image, haarFound[i], cv::Scalar(255, 255, 0), 2);
+                    break;
+                }
+            }
+            if(correct) {
+                numCorrectHaar++;
+            }
+            numHypHaar++;
+            bool otherFound = false;
+            for(size_t j = 0; j < found.size(); j++) {
+                if((found[j] & haarFound[i]).area() > found[j].area() / 4 && (found[j] & haarFound[i]).area() > haarFound[i].area() * .8) {
+                    otherFound = true;
+                    break;
+                }
+            }
+            if(!otherFound) {
+                if(correct) numCorrectBoth++;
+                numHypBoth++;
+            }
+        }
+        numRef += frame->second.size();
         if(show) {
             cv::imshow("image", image);
             cv::waitKey(0);
@@ -166,5 +262,11 @@ int main(int argc, char** argv) {
     }
     double recall = numCorrect / numRef;
     double precision = numCorrect / numHyp;
-    std::cout << numRef << " " << numHyp << " " << numCorrect << " r=" << recall << " p=" << precision << " f=" << (2 * precision * recall) / (precision + recall) << "\n";
+    std::cout << "HOG: " << numRef << " " << numHyp << " " << numCorrect << " r=" << recall << " p=" << precision << " f=" << (2 * precision * recall) / (precision + recall) << "\n";
+    recall = numCorrectHaar / numRef;
+    precision = numCorrectHaar / numHypHaar;
+    std::cout << "HAAR: " << numRef << " " << numHypHaar << " " << numCorrectHaar << " r=" << recall << " p=" << precision << " f=" << (2 * precision * recall) / (precision + recall) << "\n";
+    recall = numCorrectBoth / numRef;
+    precision = numCorrectBoth / numHypBoth;
+    std::cout << "BOTH: " << numRef << " " << numHypBoth << " " << numCorrectBoth << " r=" << recall << " p=" << precision << " f=" << (2 * precision * recall) / (precision + recall) << "\n";
 }
